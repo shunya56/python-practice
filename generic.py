fe85cefd905eae645b33f16506b2f6d0f1678233
shunya56 <s.goroku1019@gmail.com>
2022-11-16 15:01:27 +0900

    update

--- generic.py
+++ generic.py
@@ -9,7 +9,8 @@ class Sample(Generic[T]):
     Generic の使い方
     インスタンス生成時にクラスの型を決める
     """
-    def set(self, v:T):
+
+    def set(self, v: T):
         self.value: T = v
 
     def get(self) -> T:
@@ -26,6 +27,7 @@ class PairList(Generic[T1, T2]):
     Generic の使い方2
     インスタンス生成時にクラスの型を決める
     """
+
     def __init__(self):
         self._ls: List[Tuple[T1, T2]] = []
 
@@ -41,13 +43,12 @@ if __name__ == '__main__':
     sample.set(1)
     val = sample.get()
     print(type(val))
-    
-    sample.set("1")  # 警告が出る（実際はそのまま通る）
+
+    sample.set("1")   # type: ignore  # 警告が出る（実際はそのまま通る）
     val2 = sample.get()
     print(type(val2))
 
-
     # 型クラスの指定
     a: PairList[str, int] = PairList()
-    a.add(1, "")  # ここで警告が出る
+    a.add(1, "")    # type: ignore  # ここで警告が出る
     a.add("", 12)
